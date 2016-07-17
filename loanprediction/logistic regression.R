d8001f9f5ea15d94b3ad94380d4387c6aad2084f
arun <arunsadhasivam@yahoo.co.in>
2016-07-17 21:19:12 +0530

    Update logistic regression.R

--- loanprediction/logistic regression.R
+++ loanprediction/logistic regression.R
@@ -74,6 +74,6 @@ test$Loan_Status=pred
 
 #############################6. Generate Output
 write.csv(file="output.csv",c(test['Loan_ID'],test['Loan_Status']), row.names=F)
-#Random Forest      :Your score for this submission is : 0.791666666667. Your leaderboard score is : 0.805555555556
+#Random Forest      :Your score for this submission is : 0.805555555556. Your leaderboard score is : 0.805555555556
 #Logistic regression:Your score for this submission is : 0.791666666667. Your leaderboard score is : 0.805555555556
 
